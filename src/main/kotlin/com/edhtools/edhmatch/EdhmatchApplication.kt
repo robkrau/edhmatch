a7e2501f99d485a416f2b210b6eedc58521e142a
Robert Krauß <rkrauss@Roberts-Air.fritz.box>
2023-11-05 01:12:36 +0100

    Init

---
+++ src/main/kotlin/com/edhtools/edhmatch/EdhmatchApplication.kt
@@ -0,0 +1,11 @@
+package com.edhtools.edhmatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdhmatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdhmatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/edhtools/edhmatch/EdhmatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.edhtools.edhmatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdhmatchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
